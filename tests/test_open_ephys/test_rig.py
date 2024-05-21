cb995d0fd0e09f6172e7a13d9cada79466745d83
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2024-05-21 14:04:07 -0700

    feat: renames open ephys rig

--- tests/test_open_ephys/test_rig.py
+++ tests/test_open_ephys/test_rig.py
@@ -7,7 +7,7 @@ from unittest.mock import MagicMock, patch
 
 from aind_data_schema.core.rig import Rig  # type: ignore
 
-from aind_metadata_mapper.open_ephys.open_ephys_rig import OpenEphysRigEtl
+from aind_metadata_mapper.open_ephys.rig import OpenEphysRigEtl
 
 RESOURCES_DIR = (
     Path(os.path.dirname(os.path.realpath(__file__))) / ".." / "resources"
