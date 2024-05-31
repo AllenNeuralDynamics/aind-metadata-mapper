33c9a033daa4f1adb79a41eed3b23492a341e2c4
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2024-05-31 11:44:10 -0700

    fix: catches validation error

--- src/aind_metadata_mapper/gather_metadata.py
+++ src/aind_metadata_mapper/gather_metadata.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import logging
 import sys
 from pathlib import Path
 from typing import List, Optional, Type
@@ -381,7 +382,7 @@ class GatherMetadataJob:
                     contents = json.load(f)
                 try:
                     output = model.model_validate_json(json.dumps(contents))
-                except ValidationError:
+                except (ValidationError, AttributeError, ValueError, KeyError):
                     output = model.model_construct(**contents)
 
                 return output
@@ -412,19 +413,34 @@ class GatherMetadataJob:
             self.settings.metadata_settings.processing_filepath, Processing
         )
 
-        metadata = Metadata(
-            name=self.settings.metadata_settings.name,
-            location=self.settings.metadata_settings.location,
-            subject=subject,
-            data_description=data_description,
-            procedures=procedures,
-            session=session,
-            rig=rig,
-            processing=processing,
-            acquisition=acquisition,
-            instrument=instrument,
-        )
-        return metadata
+        try:
+            metadata = Metadata(
+                name=self.settings.metadata_settings.name,
+                location=self.settings.metadata_settings.location,
+                subject=subject,
+                data_description=data_description,
+                procedures=procedures,
+                session=session,
+                rig=rig,
+                processing=processing,
+                acquisition=acquisition,
+                instrument=instrument,
+            )
+            return metadata
+        except (ValidationError, ValueError, AttributeError, KeyError):
+            metadata = Metadata.model_construct(
+                name=self.settings.metadata_settings.name,
+                location=self.settings.metadata_settings.location,
+                subject=subject,
+                data_description=data_description,
+                procedures=procedures,
+                session=session,
+                rig=rig,
+                processing=processing,
+                acquisition=acquisition,
+                instrument=instrument,
+            )
+            return metadata
 
     def _write_json_file(self, filename: str, contents: dict) -> None:
         """
