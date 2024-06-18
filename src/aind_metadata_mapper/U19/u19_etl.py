12d64614e74bbdb6f5eb0f6e9c5af81fb6ae2758
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-06-18 15:40:12 -0700

    Update u19_etl.py

--- src/aind_metadata_mapper/U19/u19_etl.py
+++ src/aind_metadata_mapper/U19/u19_etl.py
@@ -27,7 +27,10 @@ from decimal import Decimal
 class JobSettings(BaseSettings):
     """Data that needs to be input by user."""
 
-    data_path: Path
+    tracking_sheet_path: Path
+    tracker_sheet_name: str
+    coordinate_sheet_path: Path
+    coordinate_sheet_name: str
     output_directory: Optional[Path] = Field(
         default=None,
         description=(
@@ -68,22 +71,91 @@ class U19Etl(GenericEtl[JobSettings]):
             job_settings_model = job_settings
         super().__init__(job_settings=job_settings_model)
 
-    def _extract(self) -> :
+    def _extract(self, subj_id) -> :
         """Extract the data from the bruker files."""
+
 
 
         return metadata
 
-    def _transform(self, input_metadata: BrukerMetadata) -> Session:
+    def _transform(self, info_row) -> Surgery:
         """Transform the data into the AIND data schema."""
 
-        return self.load_mri_session(
-            experimenter=self.job_settings.experimenter_full_name,
-            primary_scan_number=self.job_settings.primary_scan_number,
-            setup_scan_number=self.job_settings.setup_scan_number,
-            scan_data=input_metadata.scan_data,
-            subject_data=input_metadata.subject_data,
+        return Surgery(
+            start_date=,
+            experimenter_full_name=,
+            iacuc_protocol=,
+            animal_weight_prior=,
+            animal_weight_post=,
+            weight_unit=,
+            anaesthesia=,
+            workstation_id=,
+            procedures=,
+            notes=,
         )
+    
+    def download_procedure_file(self, subj_id: str):
+        """Download the procedure file for a subject."""
+        # Get the procedure file from the U19 server
+        request = requests.get(f"http://aind-metadata-service/procedures/{subj_id}")
+
+        if request.status_code == 404:
+            logging.error(f"{subj_id} model not found")
+            return None
+
+        item = request.json()
+
+        if item['message'] == 'Valid Model.':
+            return item['data']
+        return None
+    
+    def extract_procedures(self, procedure_file):
+        """Extract the procedures from the procedure file."""
+
+        return [procedure for procedure in procedure_file['subject_procedures']]
+    
+
+    def transform_perfusion(self, subj_id: str):
+        """Transform the perfusion procedure."""
+
+        return Perfusion(
+            protocol_id="dx.doi.org/10.17504/protocols.io.bg5vjy66",
+            output_specimen_ids=[subj_id]
+        )
+    
+    def transform_nanoject(self, procedure, row, injection: str):
+        """Transform the nanoject procedure."""
+
+        virus = row[injection]['Virus']
+        inj_angle = row[injection]['Inj Angle']
+        inj_vol = row[injection]['nL']
+        coord_ml = row[injection]['M/L']
+        coord_ap = row[injection]['A/P']
+        coord_depth = row[injection]['D/V']
+        hemisphere = row[injection]['Hemisphere']
+
+        virus_titer = None
+        if not pd.isna(row[injection]['Titer']):
+            virus_titer = Decimal.from_float(float(row[injection]['Titer']))
+
+        virus_lot = row[injection]['Lot']
+
+        mat1 = ViralMaterial(
+            name=virus,
+            titer=virus_titer,
+            tars_identifiers=TarsVirusIdentifiers(
+                prep_lot_number=str(virus_lot),
+            )
+        )
+
+        inj_mats = [mat1]
+
+        return NanojectInjection(
+            protocol_id="dx.doi.org/10.17504/protocols.io.bg5vjy66",
+            output_specimen_ids=[subj_id]
+        )
+
+        
 
     def run_job(self) -> JobResponse:
         """Run the job and return the response."""
