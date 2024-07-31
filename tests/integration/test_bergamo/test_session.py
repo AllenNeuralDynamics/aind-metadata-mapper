1d36d05996d527824943db3c9017979dc37d3a80
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2024-07-31 15:39:28 -0700

    assertion

--- tests/integration/test_bergamo/test_session.py
+++ tests/integration/test_bergamo/test_session.py
@@ -1,53 +1,57 @@
 """Integration test for bergamo session"""
 
-from src.aind_metadata_mapper.bergamo.session import JobSettings, BergamoEtl
+from aind_metadata_mapper.bergamo.session import JobSettings, BergamoEtl
 import s3fs
 import os
+import json
+import shutil
 
 BUCKET_NAME = os.getenv("AWS_METADATA_MAPPER_BUCKET")
-print(BUCKET_NAME)
-BASE_DIR = "metadata-mapper-integration-testing"
-BERGAMO_INPUT_SOURCE = f"{BUCKET_NAME}/{BASE_DIR}/single-plane-ophys_706957_2024-01-12_16-13-29/ophys/"
+BASE_DIR = "metadata-mapper-integration-testing/bergamo"
+BERGAMO_INPUT_SOURCE = f"{BUCKET_NAME}/{BASE_DIR}/tiff_files/"
+EXPECTED_SESSION = f"{BUCKET_NAME}/{BASE_DIR}/expected_session.json"
 LOCAL_DIR = "resources/bergamo/tiff_files/"
 
 
-def main():
-    experimenter_full_name = "Miles Morales"
-    subject_id = "706957"
+def test_bergamo_etl():
+    """Tests that BergamoETL creates session as expected."""
 
     s3fs_client = s3fs.core.S3FileSystem()
     s3_ls = s3fs_client.ls(BERGAMO_INPUT_SOURCE)
 
-    # Ensure the local directory exists
-    os.makedirs("resources/bergamo/tiff_files/", exist_ok=True)
+    os.makedirs(LOCAL_DIR, exist_ok=True)
 
+    # Copies TIFF files from s3 source
     for path in s3_ls:
-        if s3fs_client.isdir(path):
-            continue
+        if not s3fs_client.isdir(path):
+            relative_path = os.path.relpath(path, BERGAMO_INPUT_SOURCE)
+            local_file_path = os.path.join(LOCAL_DIR, relative_path)
 
-        # Construct local file path
-        relative_path = os.path.relpath(path, BERGAMO_INPUT_SOURCE)
-        local_file_path = os.path.join(LOCAL_DIR, relative_path)
+            os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+            s3fs_client.get(path, local_file_path)
 
-        # Ensure local subdirectories exist
-        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
-
-        # Download the file
-        s3fs_client.get(path, local_file_path)
-
+    # Define JobSettings and run Bergamo job
     job_settings = JobSettings(
         input_source=LOCAL_DIR,
-        experimenter_full_name=[experimenter_full_name],
-        subject_id=subject_id,
-        imaging_laser_wavelength=12,
-        fov_imaging_depth=12,
-        fov_targeted_structure="some structure",
+        experimenter_full_name=["Jane Doe"],
+        subject_id="706957",
+        imaging_laser_wavelength=405,
+        fov_imaging_depth=150,
+        fov_targeted_structure="M1",
         notes=None,
     )
     bergamo_job = BergamoEtl(job_settings=job_settings)
     response = bergamo_job.run_job()
-    print(response)
+
+    with s3fs_client.open(EXPECTED_SESSION, "r") as s3_file:
+        expected_session = json.load(s3_file)
+
+    assert json.loads(response.data) == expected_session
+
+    # Delete downloaded content
+    shutil.rmtree(LOCAL_DIR)
+    print("Cleanup complete: All downloaded files have been deleted.")
 
 
 if __name__ == "__main__":
-    main()
+    test_bergamo_etl()
