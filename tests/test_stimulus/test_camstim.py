2ef167a0cab2541425543a4ff65ac0772f78aeb7
Arielle Leon <ariellel@alleninstitute.org>
2024-11-12 12:11:38 -0800

    Re-arrange test after shrinking build_stimulus_table method.

--- tests/test_stimulus/test_camstim.py
+++ tests/test_stimulus/test_camstim.py
@@ -1,3 +1,4 @@
+"""Test the camstim.py module"""
 import unittest
 from datetime import datetime as dt
 from pathlib import Path
@@ -11,20 +12,33 @@ from aind_metadata_mapper.stimulus.camstim import Camstim, CamstimSettings
 
 
 class TestCamstim(unittest.TestCase):
+    """Test camstim.py
+    """
     @classmethod
     @patch("pathlib.Path.rglob")
     @patch("aind_metadata_mapper.stimulus.camstim.Camstim._get_sync_times")
     @patch("aind_metadata_mapper.stimulus.camstim.Camstim.get_session_uuid")
     @patch("aind_metadata_mapper.stimulus.camstim.Camstim.get_mtrain")
     @patch("aind_metadata_mapper.stimulus.camstim.Camstim._is_behavior")
+    @patch("aind_metadata_mapper.open_ephys.utils.pkl_utils.load_pkl")
+    @patch("aind_metadata_mapper.open_ephys.utils.sync_utils.load_sync")
+    @patch("aind_metadata_mapper.open_ephys.utils.pkl_utils.get_fps")
     def setUpClass(
         cls,
+        mock_get_fps: MagicMock,
+        mock_load_sync: MagicMock,
+        mock_load_pkl: MagicMock,
         mock_is_behavior: MagicMock,
         mock_mtrain: MagicMock,
         mock_session_uuid: MagicMock,
         mock_sync_times: MagicMock,
         mock_rglob: MagicMock,
     ) -> None:
+        """Set up the test suite
+        """
+        mock_get_fps.return_value = 30.0
+        mock_load_sync.return_value = {}
+        mock_load_pkl.return_value  = {"fps": 30.0}
         mock_is_behavior.return_value = True
         mock_mtrain.return_value = {
             "name": "test_name",
@@ -52,10 +66,10 @@ class TestCamstim(unittest.TestCase):
         )
 
     @patch(
-        "aind_metadata_mapper.stimulus.camstim.sync.get_ophys_stimulus_timestamps"
+        "aind_metadata_mapper.stimulus.camstim.sync.get_ophys_stimulus_timestamps" #noqa
     )
     @patch(
-        "aind_metadata_mapper.stimulus.camstim.behavior_utils.from_stimulus_file"
+        "aind_metadata_mapper.stimulus.camstim.behavior_utils.from_stimulus_file" #noqa
     )
     @patch("pandas.DataFrame.to_csv")
     def test_build_behavior_table(
@@ -64,6 +78,7 @@ class TestCamstim(unittest.TestCase):
         mock_from_stimulus_file: MagicMock,
         mock_get_ophys_stimulus_timestamps: MagicMock,
     ):
+        """Test the build_behavior_table method"""
         # Mock the return values
         mock_get_ophys_stimulus_timestamps.return_value = [1, 2, 3]
         mock_from_stimulus_file.return_value = [pd.DataFrame({"a": [1, 2, 3]})]
@@ -82,112 +97,56 @@ class TestCamstim(unittest.TestCase):
             self.camstim.stim_table_path, index=False
         )
 
-    @patch("aind_metadata_mapper.stimulus.camstim.pkl.get_fps")
-    @patch("aind_metadata_mapper.stimulus.camstim.pkl.load_pkl")
-    @patch("aind_metadata_mapper.stimulus.camstim.sync.load_sync")
     @patch(
-        "aind_metadata_mapper.stimulus.camstim.stim_utils.extract_frame_times_from_photodiode"
-    )
-    @patch(
-        "aind_metadata_mapper.stimulus.camstim.stim_utils.build_stimuluswise_table"
-    )
-    @patch(
-        "aind_metadata_mapper.stimulus.camstim.stim_utils.make_spontaneous_activity_tables"
+        "aind_metadata_mapper.stimulus.camstim.stim_utils.extract_frame_times_from_photodiode" #noqa
     )
     @patch(
         "aind_metadata_mapper.stimulus.camstim.stim_utils.create_stim_table"
     )
-    @patch(
-        "aind_metadata_mapper.stimulus.camstim.stim_utils.convert_frames_to_seconds"
-    )
-    @patch("aind_metadata_mapper.stimulus.camstim.names.collapse_columns")
-    @patch("aind_metadata_mapper.stimulus.camstim.names.drop_empty_columns")
-    @patch(
-        "aind_metadata_mapper.stimulus.camstim.names.standardize_movie_numbers"
-    )
-    @patch(
-        "aind_metadata_mapper.stimulus.camstim.names.add_number_to_shuffled_movie"
-    )
-    @patch("aind_metadata_mapper.stimulus.camstim.names.map_stimulus_names")
     @patch("aind_metadata_mapper.stimulus.camstim.names.map_column_names")
     @patch("pandas.DataFrame.to_csv")
-    @patch(
-        "aind_metadata_mapper.stimulus.camstim.stim_utils.build_stimuluswise_table"
-    )
     @patch(
         "aind_metadata_mapper.stimulus.camstim.stim_utils.seconds_to_frames"
     )
     @patch("aind_metadata_mapper.open_ephys.utils.pkl_utils.get_stimuli")
     @patch(
-        "aind_metadata_mapper.open_ephys.utils.stim_utils.extract_blocks_from_stim"
+        "aind_metadata_mapper.open_ephys.utils.stim_utils.extract_blocks_from_stim" #noqa
     )
+    @patch("aind_metadata_mapper.stimulus.camstim.Camstim.get_stim_table_seconds")
     def test_build_stimulus_table(
         self,
+        mock_get_stim_table_seconds: MagicMock,
         mock_extract_blocks_from_stim: MagicMock,
         mock_get_stimuli: MagicMock,
         mock_seconds_to_frames: MagicMock,
-        mock_stimwise_table: MagicMock,
         mock_to_csv: MagicMock,
         mock_map_column_names: MagicMock,
-        mock_map_stimulus_names: MagicMock,
-        mock_add_number_to_shuffled_movie: MagicMock,
-        mock_standardize_movie_numbers: MagicMock,
-        mock_drop_empty_columns: MagicMock,
-        mock_collapse_columns: MagicMock,
-        mock_convert_frames_to_seconds: MagicMock,
         mock_create_stim_table: MagicMock,
-        mock_make_spontaneous_activity_tables: MagicMock,
-        mock_build_stimuluswise_table: MagicMock,
         mock_extract_frame_times_from_photodiode: MagicMock,
-        mock_load_sync: MagicMock,
-        mock_load_pkl: MagicMock,
-        mock_get_fps: MagicMock,
     ):
+        """Test the build_stimulus_table method"""
         # Mock the return values
+        mock_get_stim_table_seconds.return_value = [pd.DataFrame({"a": [1, 2, 3]})]
         mock_extract_blocks_from_stim.return_value = [1, 2, 3]
         mock_get_stimuli.return_value = {"stuff": "things"}
         mock_seconds_to_frames.return_value = np.array([1, 2, 3])
-        mock_stimwise_table.return_value = [pd.DataFrame({"a": [1, 2, 3]})]
-        mock_get_fps.return_value = 30.0
-        mock_load_sync.return_value = {"some_sync_key": "some_sync_value"}
         mock_extract_frame_times_from_photodiode.return_value = [0.1, 0.2, 0.3]
         mock_create_stim_table.return_value = pd.DataFrame({"a": [1, 2, 3]})
-        mock_convert_frames_to_seconds.return_value = pd.DataFrame(
-            {"a": [1, 2, 3]}
-        )
-        mock_collapse_columns.return_value = pd.DataFrame({"a": [1, 2, 3]})
-        mock_drop_empty_columns.return_value = pd.DataFrame({"a": [1, 2, 3]})
-        mock_standardize_movie_numbers.return_value = pd.DataFrame(
-            {"a": [1, 2, 3]}
-        )
-        mock_add_number_to_shuffled_movie.return_value = pd.DataFrame(
-            {"a": [1, 2, 3]}
-        )
-        mock_map_stimulus_names.return_value = pd.DataFrame({"a": [1, 2, 3]})
         mock_map_column_names.return_value = pd.DataFrame({"a": [1, 2, 3]})
 
         # Call the method
         self.camstim.build_stimulus_table()
 
         # Assert the calls
-        mock_load_pkl.assert_called_once_with(self.camstim.pkl_path)
-        mock_load_sync.assert_called_once_with(self.camstim.sync_path)
-        mock_extract_frame_times_from_photodiode.assert_called_once_with(
-            mock_load_sync.return_value
-        )
+        mock_extract_frame_times_from_photodiode.assert_called_once()
         mock_create_stim_table.assert_called_once()
-        mock_convert_frames_to_seconds.assert_called_once()
-        mock_collapse_columns.assert_called_once()
-        mock_drop_empty_columns.assert_called_once()
-        mock_standardize_movie_numbers.assert_called_once()
-        mock_add_number_to_shuffled_movie.assert_called_once()
-        mock_map_stimulus_names.assert_called_once()
         mock_map_column_names.assert_called_once()
         mock_to_csv.assert_called_once_with(
             self.camstim.stim_table_path, index=False
         )
 
     def test_extract_stim_epochs(self):
+        """Test the extract_stim_epochs method"""
         # Create a mock stimulus table
         data = {
             "start_time": [0, 1, 2, 3, 4],
@@ -214,6 +173,7 @@ class TestCamstim(unittest.TestCase):
         self.assertEqual(epochs, expected_epochs)
 
     def test_extract_stim_epochs_with_images_and_movies(self):
+        """Test the extract_stim_epochs method with images and movies"""
         # Create a mock stimulus table with images and movies
         data = {
             "start_time": [0, 1, 2, 3, 4],
@@ -246,6 +206,7 @@ class TestCamstim(unittest.TestCase):
     def test_epochs_from_stim_table(
         self, mock_read_csv: MagicMock, mock_extract_stim_epochs: MagicMock
     ):
+        """Test the epochs_from_stim_table method"""
         # Mock the return values
         mock_read_csv.return_value = pd.DataFrame(
             {
