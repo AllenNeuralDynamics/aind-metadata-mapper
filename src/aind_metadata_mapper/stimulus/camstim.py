f054a7cf431abb5183dcc80e6713cc5e6c7db8ed
rcpeene <carter.peene@alleninstitute.org>
2025-07-23 14:03:56 -0700

    use lims project id to determine whether to summarize all stim in one whole session epoch

--- src/aind_metadata_mapper/stimulus/camstim.py
+++ src/aind_metadata_mapper/stimulus/camstim.py
@@ -36,6 +36,7 @@ class CamstimSettings(BaseModel):
     output_directory: Optional[Path]
     session_id: str
     subject_id: str
+    lims_project_code: Optional[str] = ""
 
 
 class Camstim:
@@ -287,6 +288,31 @@ class Camstim:
                 elif param_set:
                     current_epoch[3][column] = param_set
 
+
+    def extract_whole_session_epoch(
+        self, stim_table: pd.DataFrame
+    ) -> list[list[str, int, int, dict, set]]:
+        row = stim_table.iloc[0]
+        single_epoch = [
+            stim_table["stim_name"].iloc[0],
+            stim_table["start_time"].iloc[0],
+            stim_table["stop_time"].iloc[-1],
+            {},
+            set(),
+        ]
+        self._summarize_epoch_params(
+            stim_table, single_epoch, 0, -1
+        )
+        stim_name = row.get("stim_name", "") or ""
+        image_set = row.get("image_set", "")
+        if pd.notnull(image_set):
+            stim_name = image_set
+
+        if "image" in stim_name.lower() or "movie" in stim_name.lower():
+            single_epoch[4].add(row["stim_name"])
+        return [single_epoch]
+
+
     def extract_stim_epochs(
         self, stim_table: pd.DataFrame
     ) -> list[list[str, int, int, dict, set]]:
@@ -300,10 +326,13 @@ class Camstim:
         stim_name, stim_type, or frame) are listed as parameters, and the set
         of values for that column are listed as parameter values.
         """
+        if self.camstim_settings.lims_project_code == "U01BFCT":
+            return self.extract_whole_session_epoch(stim_table)
+
         epochs = []
         current_epoch = [None, 0.0, 0.0, {}, set()]
         epoch_start_idx = 0
-        for current_idx, row in stim_table.iterrows():
+        for current_idx, row in stim_table.iterrows():            
             if row["stim_name"] == "spontaneous" or row['start_time'] == current_epoch[1]:
                 continue
             if row["stim_name"] != current_epoch[0]:
