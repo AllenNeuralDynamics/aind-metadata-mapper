4d9abc4ab3d88f4cc6608c190e0072f6a3f87e7f
Dan Birman <danbirman@gmail.com>
2025-08-27 13:03:15 -0700

    refactor: start clearing out mapper to re-build for v2

--- src/aind_metadata_mapper/smartspim/mapper.py
+++ src/aind_metadata_mapper/smartspim/mapper.py
@@ -1,7 +1,13 @@
-from datetime import datetime
-import re
 from aind_data_schema.core.acquisition import Acquisition
-from aind_metadata_extractor.models import SmartspimModel
+from aind_data_schema.components.configs import (
+    ImagingConfig,
+    Channel,
+    DetectorConfig,
+    LightEmittingDiodeConfig,
+    LaserConfig,
+    DeviceConfig,
+)
+from aind_metadata_extractor.models.smartspim import SmartspimModel
 from aind_metadata_mapper.models import Mapper
 
 
@@ -9,77 +15,76 @@ class SmartspimMapper(Mapper):
     """Smartspim Mapper"""
     
     def _transform(self, metadata: SmartspimModel) -> Acquisition:
-    """
-    Transforms raw metadata from both microscope files and SLIMS
-    into a complete Acquisition model.
+        """
+        Transforms raw metadata from both microscope files and SLIMS
+        into a complete Acquisition model.
 
-    Parameters
-    ----------
-    metadata_dict : Dict
-        Metadata extracted from the microscope files.
-    slims_data : Dict
-        Metadata fetched from the SLiMS service.
+        Parameters
+        ----------
+        metadata_dict : Dict
+            Metadata extracted from the microscope files.
+        slims_data : Dict
+            Metadata fetched from the SLiMS service.
 
-    Returns
-    -------
-    Acquisition
-        Fully composed acquisition model.
-    """
-    mdate_match = re.search(
-        self.REGEX_DATE, self.job_settings.input_source.stem
-    )
-    mid_match = re.search(
-        self.REGEX_MOUSE_ID, self.job_settings.input_source.stem
-    )
-    if not (mdate_match and mid_match):
-        raise ValueError("Error while extracting mouse date and ID")
-    session_start = datetime.strptime(
-        mdate_match.group(), "%Y-%m-%d_%H-%M-%S"
-    )
-    subject_id = mid_match.group()
+        Returns
+        -------
+        Acquisition
+            Fully composed acquisition model.
+        """
+        subject_id = metadata.slims_metadata.subject_id
 
-    # fields from metadata_dict
-    active_obj = metadata_dict["session_config"].get("Obj")
+        # fields from metadata_dict
+        active_obj = metadata.file_metadata.session_config.get("Obj")
 
-    # fields from slims_data
-    specimen_id = slims_data.get("specimen_id", "")
-    instrument_id = slims_data.get("instrument_id")
-    protocol_id = slims_data.get("protocol_id")
-    experimenter_name = slims_data.get("experimenter_name")
+        # fields from slims_data
+        specimen_id = metadata.slims_metadata.specimen_id
+        instrument_id = metadata.slims_metadata.instrument_id
+        protocol_id = metadata.slims_metadata.protocol_id
+        experimenter_name = metadata.slims_metadata.experimenter_name
+        
+        # Build the channels
+        
+        
+        # Build the data stream
+        
+        imaging_config = ImagingConfig(
+            
+        )
 
-    acquisition = Acquisition(
-        specimen_id=specimen_id,
-        subject_id=subject_id,
-        session_start_time=session_start,
-        session_end_time=metadata_dict["session_end_time"],
-        tiles=make_acq_tiles(
-            metadata_dict=metadata_dict,
-            filter_mapping=metadata_dict["filter_mapping"],
-        ),
-        external_storage_directory="",
-        active_objectives=[active_obj] if active_obj else None,
-        instrument_id=instrument_id,
-        experimenter_full_name=(
-            [experimenter_name] if experimenter_name else []
-        ),
-        protocol_id=[protocol_id] if protocol_id else [],
-        chamber_immersion=Immersion(
-            medium=self._map_immersion_medium(
-                slims_data.get("chamber_immersion_medium")
+        acquisition = Acquisition(
+            specimen_id=specimen_id,
+            subject_id=subject_id,
+            acquisition_type="SmartSPIM",
+            acquisition_start_time=metadata.file_metadata.session_start_time,
+            acquisition_end_time=metadata.file_metadata.session_end_time,
+            tiles=make_acq_tiles(
+                metadata_dict=metadata_dict,
+                filter_mapping=metadata_dict["filter_mapping"],
             ),
-            refractive_index=slims_data.get("chamber_refractive_index"),
-        ),
-        sample_immersion=Immersion(
-            medium=self._map_immersion_medium(
-                slims_data.get("sample_immersion_medium")
+            external_storage_directory="",
+            active_objectives=[active_obj] if active_obj else None,
+            instrument_id=instrument_id,
+            experimenter_full_name=(
+                [experimenter_name] if experimenter_name else []
             ),
-            refractive_index=slims_data.get("sample_refractive_index"),
-        ),
-        axes=self._map_axes(
-            x=slims_data.get("x_direction"),
-            y=slims_data.get("y_direction"),
-            z=slims_data.get("z_direction"),
-        ),
-        processing_steps=self._map_processing_steps(slims_data),
-    )
-    return acquisition
+            protocol_id=[protocol_id] if protocol_id else [],
+            chamber_immersion=Immersion(
+                medium=self._map_immersion_medium(
+                    slims_data.get("chamber_immersion_medium")
+                ),
+                refractive_index=slims_data.get("chamber_refractive_index"),
+            ),
+            sample_immersion=Immersion(
+                medium=self._map_immersion_medium(
+                    slims_data.get("sample_immersion_medium")
+                ),
+                refractive_index=slims_data.get("sample_refractive_index"),
+            ),
+            axes=self._map_axes(
+                x=slims_data.get("x_direction"),
+                y=slims_data.get("y_direction"),
+                z=slims_data.get("z_direction"),
+            ),
+            processing_steps=self._map_processing_steps(slims_data),
+        )
+        return acquisition
