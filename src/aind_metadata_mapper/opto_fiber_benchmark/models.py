b7d114738a1156a815eb5caf9f13955b2c48a64f
arjunsridhar12345 <arjun.sridhar1998@gmail.com>
2025-10-07 14:41:23 -0700

    feat: add initial model for opto fiber benchmark

---
+++ src/aind_metadata_mapper/opto_fiber_benchmark/models.py
@@ -0,0 +1,50 @@
+from aind_metadata_mapper.fip.models import JobSettings as FiberJobSettings
+from aind_metadata_mapper.core_models import BaseJobSettings
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from typing import List, Literal, Optional
+
+
+class OptoJobSettings(BaseJobSettings):
+    """Parameters for extracting from raw data."""
+
+    data_directory: str = Field(
+        ...,
+        description="Path to data directory",
+    )
+
+    # Optogenetics parameters
+    stimulus_name: str = Field(default="OptoStim", description="Stimulus name")
+    pulse_shape: str = Field(default="Square", title="Pulse shape")
+    pulse_frequency: List[float] = Field(..., title="Pulse frequency (Hz)")
+    number_pulse_trains: List[int] = Field(..., title="Number of pulse trains")
+    pulse_width: List[int] = Field(..., title="Pulse width (ms)")
+
+    pulse_train_duration: List[float] = Field(
+        ..., title="Pulse train duration (s)"
+    )
+    fixed_pulse_train_interval: bool = Field(
+        default=True, title="Fixed pulse train interval"
+    )
+    pulse_train_interval: Optional[float] = Field(
+        ...,
+        title="Pulse train interval (s)",
+        description="Time between pulse trains",
+    )
+    baseline_duration: float = Field(
+        ...,
+        title="Baseline duration (s)",
+        description="Duration of baseline recording prior to first pulse train",
+    )
+
+    # Stimulus epoch laser configs
+    wavelength: int = Field(..., title="Wavelength (nm)")
+    power: float = Field(..., title="Excitation power")
+
+class JobSettings(BaseJobSettings, BaseSettings, cli_parse_args=True):
+    job_settings_name: Literal["Opto_Fiber_Benchmark"] = Field(
+        default="Opto_Fiber_Benchmark", title="Name of the job settings"
+    )
+    fiber: FiberJobSettings = Field(default_factory=FiberJobSettings)
+    opto: OptoJobSettings = Field(default_factory=OptoJobSettings)
